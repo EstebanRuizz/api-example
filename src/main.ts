1ab8d0410562f892a257deffd24f8fe20dd4688f
EstebanRuizz <rubianruiz05@my.com>
2024-05-22 07:28:10 -0500

    Added interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './presentation/interceptors/LoggingInterceptor';
+import { TransformInterceptor } from './presentation/interceptors/TransformInterceptor';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.useGlobalInterceptors(new TransformInterceptor());
 
   await app.listen(3000);
 }
