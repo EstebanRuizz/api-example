3f26f354f5dd066154e05951acbdba803a0b4c3c
EstebanRuizz <rubianruiz05@my.com>
2024-05-22 07:56:48 -0500

    Feature: added LoggerMiddleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ProductService } from './core/application/services/product/product.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './core/domain/models/User';
 import { UserService } from './core/application/services/user/user.service';
 import { UserController } from './presentation/controllers/user/user.controller';
 import { SqliteConfig } from './infrastructure/persistence/SqliteConfig';
+import { LoggerMiddleware } from './presentation/middleware/LoggerMiddleware';
 
 @Module({
   imports: [
@@ -14,4 +15,8 @@ import { SqliteConfig } from './infrastructure/persistence/SqliteConfig';
   controllers: [UserController],
   providers: [ProductService, UserService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('user');
+  }
+}
