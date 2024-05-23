8d3555f07d5b7aa64c51b03b7933527dd8fc9054
EstebanRuizz <rubianruiz05@my.com>
2024-05-23 08:31:26 -0500

    JwtModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,23 @@ import { UserService } from './core/application/services/user/user.service';
 import { UserController } from './presentation/controllers/user/user.controller';
 import { SqliteConfig } from './infrastructure/persistence/SqliteConfig';
 import { LoggerMiddleware } from './presentation/middleware/LoggerMiddleware';
+import { AuthController } from './presentation/controllers/auth/auth.controller';
+import { AuthService } from './core/application/services/auth/auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './infrastructure/auth/constants';
 
 @Module({
   imports: [
     SequelizeModule.forRoot(SqliteConfig.getSequelizeModuleOptions()),
     SequelizeModule.forFeature([User]),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
   ],
-  controllers: [UserController],
-  providers: [ProductService, UserService],
+  controllers: [UserController, AuthController],
+  providers: [ProductService, UserService, AuthService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
