45a1101ad633ffdcd48156d692c85ba5a020122a
EstebanRuizz <rubianruiz05@my.com>
2024-06-04 02:41:18 -0500

    UserService

--- src/core/application/services/user/user.service.ts
+++ src/core/application/services/user/user.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { UserConfig } from '../../../../infrastructure/persistence/Sqlite/config/UserConfig';
 import { UserDTO } from '../../DTO/UserDTO';
+import { EnumDatabase } from '../../enums/EnumDatabase';
+import { Sequelize, WhereOptions } from 'sequelize';
 
 @Injectable()
 export class UserService {
   constructor(
-    @InjectModel(UserConfig)
+    @InjectModel(UserConfig, EnumDatabase.mssqlConnection)
     private readonly userModel: typeof UserConfig,
+    @Inject('SEQUELIZE') private readonly sequelize: Sequelize,
   ) {}
 
   findAll(): Promise<UserConfig[]> {
@@ -29,13 +32,44 @@ export class UserService {
     }
   }
 
-  public async create(userDTO: UserDTO): Promise<UserConfig> {
+  public async create(userDTO: UserDTO): Promise<UserConfig | null> {
+    const transaction = await this.sequelize.transaction();
+    try {
+      if (await this.tryGetUser(userDTO, transaction)) {
+        await transaction.rollback();
+        return null;
+      }
+      const createdUser = await this.createNewUser(userDTO, transaction);
+
+      await transaction.commit();
+
+      return createdUser;
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
+  }
+
+  private async createNewUser(userDTO: UserDTO, transaction) {
     const user: UserConfig = this.userModel.build({
-      name: userDTO.name,
+      name: userDTO.name ?? userDTO.email.split('@').shift(),
       email: userDTO.email,
       password: userDTO.password,
       roleFK: userDTO.roleFK,
     });
-    return user.save();
+
+    const createdUser = await user.save({ transaction });
+    return createdUser;
+  }
+
+  private async tryGetUser(userDTO: UserDTO, transaction) {
+    return await this.userModel.findOne({
+      where: { email: userDTO.email },
+      transaction,
+    });
+  }
+
+  public async findByExpression(where: WhereOptions): Promise<UserConfig[]> {
+    return this.userModel.findAll({ where });
   }
 }
