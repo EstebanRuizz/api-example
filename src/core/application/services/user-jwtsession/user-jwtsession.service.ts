dc42d74496c57e62ba53fbebc09efe238130375a
EstebanRuizz <rubianruiz05@my.com>
2024-06-05 11:39:45 -0500

    UserJwtSessionService

--- src/core/application/services/user-jwtsession/user-jwtsession.service.ts
+++ src/core/application/services/user-jwtsession/user-jwtsession.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
-import { UserJWTSessionConfig } from 'src/infrastructure/persistence/Sqlite/config/UserJWTSessionConfig';
+import { UserJWTSessionConfig } from 'src/infrastructure/persistence/SqlServer/config/UserJWTSessionConfig';
 
 @Injectable()
 export class UserJwtSessionService {
