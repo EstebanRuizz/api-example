d1a0b4ce801ead9ebee792ccd862ee175fd3f297
EstebanRuizz <rubianruiz05@my.com>
2024-06-05 11:36:59 -0500

    LocalUserJwtSessionService

---
+++ src/core/application/services/localDatabase/local-user-jwtsession/local-user-jwtsession.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Guid } from 'guid-typescript';
+import { EnumDatabase } from 'src/core/application/enums/EnumDatabase';
+import { LocalUserJWTSession } from 'src/infrastructure/persistence/Sqlite/config/LocalUserJWTSession';
+import { UserConfig } from 'src/infrastructure/persistence/SqlServer/config/UserConfig';
+
+@Injectable()
+export class LocalUserJwtSessionService {
+  public constructor(
+    @InjectModel(LocalUserJWTSession, EnumDatabase.sqliteConnection)
+    private readonly localUserJWTSessionModel: typeof LocalUserJWTSession,
+  ) {}
+
+  public async createJWTSessionId(
+    user: UserConfig,
+  ): Promise<LocalUserJWTSession> {
+    return await this.localUserJWTSessionModel.create({
+      jwtSessionId: Guid.create().toString(),
+      userId: user.id,
+      roleFK: user.roleFK,
+    });
+  }
+}
