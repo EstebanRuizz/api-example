8e541fa90f4c3aab2342a66eddc053aefe0244b5
EstebanRuizz <rubianruiz05@my.com>
2024-06-05 11:41:13 -0500

    AuthGuard

--- src/presentation/guards/auth/auth.guard.ts
+++ src/presentation/guards/auth/auth.guard.ts
@@ -4,35 +4,53 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
 import { jwtConstants } from 'src/infrastructure/auth/constants';
+import { LocalEntitiesByRoleService } from 'src/core/application/services/localDatabase/local-entities-by-role/local-entities-by-role.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly jwtService: JwtService) {}
+  public constructor(
+    private readonly jwtService: JwtService,
+    private readonly localEntitiesByRoleService: LocalEntitiesByRoleService,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    this.getEndPointName(context);
     const request = context.switchToHttp().getRequest();
+
     const token = this.extractTokenFromHeader(request);
     if (!token) {
       throw new UnauthorizedException();
     }
-    try {
-      const payload = await this.jwtService.verifyAsync(token, {
-        secret: jwtConstants.secret,
-      });
-      // 💡 We're assigning the payload to the request object here
-      // so that we can access it in our route handlers
-      request['user'] = payload;
-    } catch {
-      throw new UnauthorizedException();
-    }
+    await this.verifyToken(token, request);
     return true;
   }
 
   private extractTokenFromHeader(request: Request): string | undefined {
     const [type, token] = request.headers.authorization?.split(' ') ?? [];
     return type === 'Bearer' ? token : undefined;
+  }
+
+  private async verifyToken(token: string, request: Request): Promise<void> {
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: jwtConstants.secret,
+      });
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+  }
+
+  private getEndPointName(context: ExecutionContext): void {
+    const request = context.switchToHttp().getRequest();
+    const pathId: string = [
+      `[${request.method}]`,
+      `[${Reflect.getMetadata('path', context.getClass())}]`,
+      `[${request.url}]`,
+    ].join('');
+    console.log(pathId);
   }
 }
